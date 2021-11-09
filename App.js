692c80f55c696e805f5d7acbf443641afa81efd5
Gaston Ezequiel Cohan <gascohan@BAI-C02TLDQPG8WN.local>
2021-11-08 22:00:39 -0300

    lista funcionable de categorias

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Input, NativeBaseProvider, IconButton, Icon } from "native-base"
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Input, NativeBaseProvider, IconButton, Icon, FlatList } from "native-base"
 import { Ionicons } from "@expo/vector-icons"
 
 export default function App() {
 
   // States
 
+  const [textValue, setTextValue] = useState('')
+  const [itemList, setItemList] = useState([])
 
   // Methods
 
+  const handleChangeText = (value) => {
+    setTextValue(value)
+  }
+
+  const HandleAddItem = () => {
+    setTextValue('');
+    const item = {
+      value: textValue,
+      id: Math.random().toString(),
+    };
+    setItemList([
+      ...itemList,
+      item
+    ]);
+  }
+
+  // const handleModal = id => {
+  //   setItemSelected(itemList.find(item => item.id === id));
+  //   setModalVisible(true)
+  // }
 
   return (
     <View style={styles.container}>
@@ -18,11 +40,27 @@ export default function App() {
           <Input
             placeholder="Ingresa una categoria"
             style={{ justifyContent: "center", textAlign: "center" }}
+            onChangeText={handleChangeText}
+            value={textValue}
           />
-          <IconButton icon={<Icon as={Ionicons} name="send" />} />
+          <IconButton icon={<Icon as={Ionicons} name="send" onPress={HandleAddItem} />} />
         </View>
         <View style={{ marginTop: '15%' }}>
           <Text> Categorias añadidas</Text>
+          <FlatList
+            data={itemList}
+            renderItem={data => {
+              return (
+                <View>
+                  <Text>{data.item.value}</Text>
+                  {/* <Button title="X" color="#AAAAAA" onPress={() => handleModal(data.item.id)} /> */}
+                </View>
+              )
+            }}
+            keyExtractor={item => item.id}
+          />
+
+
         </View>
       </NativeBaseProvider>
     </View>
